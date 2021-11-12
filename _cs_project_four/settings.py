8ff233d0f3f385140b017dbe1af1fc17a4bab0f4
tanley5 <tanleybench@gmail.com>
2021-11-12 09:48:13 -0700

    Added Email and Environment Variables

--- _cs_project_four/settings.py
+++ _cs_project_four/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(rv3dr@_9+=m!-5!mzzrivwrw55)z#y^5m1t(*zm4r@_eu5x-o'
+# SECRET_KEY = 'django-insecure-(rv3dr@_9+=m!-5!mzzrivwrw55)z#y^5m1t(*zm4r@_eu5x-o'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -141,3 +146,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Django Crispy Forms Template Pack
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
+# Email Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+
+RECIPIENT_ADDRESS = env('RECIPIENT_ADDRESS')
